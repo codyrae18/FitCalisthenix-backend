ea8913aa6a0e17882fdf6f67134db8f165e39c63
Cody <lawdermiltcody@gmail.com>
2019-10-23 10:36:19 -0500

    updated migration & controllers

--- db/migrate/20191021175225_users.rb
+++ db/migrate/20191021175225_users.rb
@@ -2,11 +2,12 @@ class Users < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :email
+      t.string :password
       t.integer :age
       t.integer :height
       t.integer :weight
       t.string :photo
-      t.integer :experience
 
       t.timestamps
     end
